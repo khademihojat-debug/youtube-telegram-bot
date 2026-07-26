9b7e24c8fb057225ccb2a5a8ebfc1f818029a7ae
khademihojat-debug <khademi.hojat@gmail.com>
2026-07-26 23:41:40 +0330

    Create Keyboards.py

---
+++ Keyboards.py
@@ -0,0 +1,9 @@
+from telegram import ReplyKeyboardMarkup
+
+main_keyboard = ReplyKeyboardMarkup(
+    [
+        ["🎬 Download Video", "🎵 Download MP3"],
+        ["ℹ️ Help"],
+    ],
+    resize_keyboard=True
+)

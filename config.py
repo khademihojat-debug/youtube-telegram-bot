e8e1df4f52d3a247e6afcb15bf01c23cee651eda
khademihojat-debug <khademi.hojat@gmail.com>
2026-07-28 07:18:36 +0330

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-
-DB_PATH = "data.db"
-DOWNLOAD_DIR = "downloads"
-MAX_DAILY_DOWNLOADS = 15
-TELEGRAM_FILE_LIMIT_MB = 48
